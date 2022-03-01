586fe6ec45418305e2487c0437fbc5139b21ffd0
Miguel Angel Quispe Mamani <99612891+MiguelAngelQ@users.noreply.github.com>
2022-02-28 23:26:02 -0400

    Add files via upload

---
+++ Clase 26-02-2022/C#/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Micky
+ * Date: 28/2/2022
+ * Time: 22:16
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Main{
+	class Program{
+		public static void Main(string[] args){
+			mochila a = new mochila();//con constructor por defecto
+			a.mostrar();
+			mochila m = new mochila("Adidas", 200, "naranjado", 10.2);
+			m.mostrar();
+			mochila h = new mochila();
+			h.leer();
+			h.mostrar();
+			
+			Console.ReadKey(true);
+		}
+	}
+}
